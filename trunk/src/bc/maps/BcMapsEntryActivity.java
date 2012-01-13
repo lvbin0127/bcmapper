44613f3affe7a8cac233663547c6ddd8b6cb1dd9
csomerlot <csomerlot@867dcdde-f414-a652-aa54-acb54947a79e>
2012-01-13 14:49:24 +0000

    Added GPL2 license

--- trunk/src/bc/maps/BcMapsEntryActivity.java
+++ trunk/src/bc/maps/BcMapsEntryActivity.java
@@ -1,3 +1,32 @@
+
+/*
+bcMapper is a mapping application for displaying raster and vector data on the Android platform.
+
+
+Copyright (C) Brown and Caldwell, 2012
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+For more information, contact:
+
+    Chris Somerlot
+    Brown and Caldwell
+    290 Elwood Davis Rd, Suite 290
+    Liverpool, NY 13088
+    
+*/
+
 package bc.maps;
 
 import android.app.Activity;
